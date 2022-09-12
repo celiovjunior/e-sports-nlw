b51a8234378f75bf4eaeb148c43c60903c4d2b93
Celio Junior <celio_cjr@hotmail.com>
2022-09-12 19:53:41 -0300

    installing tailwindcss

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,11 @@
+import './styles/main.css';
+
 function App() {
-  return <h1>hellooooooo</h1>
+    return (
+        <div className="w-8 h-8 bg-black dark:bg-slate-500">
+
+        </div>
+    )
 }
 
 export default App
