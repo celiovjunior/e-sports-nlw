104c36be5db4eb8211b87c234816518ad2bdfe11
Celio Junior <celio_cjr@hotmail.com>
2022-09-13 22:55:54 -0300

    installed prisma, sqlite, studied the prisma doc, configured the prisma file, made the routes and tested the request until get ads by game id

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,46 @@
-import express from 'express'
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
-const app = express()
+const app = express();
+const prisma = new PrismaClient();
 
-app.get('/ads', (req, res) => {
-    return res.json([
-        { id: 1, name: 'Anuncio 1' },
-        { id: 2, name: 'Anuncio 2' },
-        { id: 4, name: 'Anuncio 4' },
-        { id: 5, name: 'Anuncio 5' },
-    ])
+app.get('/games', async (request, response) => {
+
+	const games = await prisma.game.findMany({
+		include: {
+			_count: {
+				select: {
+					ads: true
+				}
+			}
+		}
+	});
+
+	return response.json(games);
+});
+
+
+app.post('/ads', (request, response) => {
+	return response.status(201).json([]);
+})
+
+app.get('/games/:id/ads', async (request, response) => {
+	const gameId = request.params.id;
+
+	const ads = await prisma.ad.findMany({
+		where: {
+			gameId
+		}
+	})
+
+	return response.json(ads)
+})
+
+app.get('/ads/:id/discord', (request, response) => {
+
+	const adId = request.params.id;
+
+	return response.json([]);
 })
 
 app.listen(3333)
